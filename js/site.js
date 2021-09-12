795c2f203dbea65f9b6d2f61bf68168fe7c36051
fullstackjunkie <rongoodwindev@gmail.com>
2021-09-12 15:51:04 -0400

    Building Content

--- js/site.js
+++ js/site.js
@@ -1,4 +1,23 @@
-//Call Hello World
-const HelloWorld = () => {
-    alert("Hello, World");
+
+//gets the data needed
+function getData() {
+    let loanAmount = document.getElementById("loan-amount").value;
+    let terms = document.getElementById("terms").value;
+    let interest = document.getElementById("interest").value;
+
+    //make sure they are values
+    loanAmount = 
+    console.log(`${loanAmount} ${terms} ${interest}`);
+}
+
+
+//solves the issue needed
+function loanShark() {
+
+}
+
+
+//displays the data
+function displayData() {
+
 }
