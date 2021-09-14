2c1b2c5912c75bc36077861d495838a6a89297db
fullstackjunkie <rongoodwindev@gmail.com>
2021-09-13 20:19:38 -0400

    Still In Progress

--- js/site.js
+++ js/site.js
@@ -6,18 +6,40 @@ function getData() {
     let interest = document.getElementById("interest").value;
 
     //make sure they are values
-    loanAmount = 
-    console.log(`${loanAmount} ${terms} ${interest}`);
+    loanAmount = parseFloat(loanAmount);
+    terms = parseInt(terms);
+    interest = parseFloat(interest);
+    
+    //calling the logic
+    let loanContent = loanShark(loanAmount, terms, interest);
+
+    //calling the display
+    displayData(loanContent);
 }
 
 
 //solves the issue needed
-function loanShark() {
+function loanShark(loan, term, interest) {
 
+    let totalMonthPayment = loan * ((interest / 1200)/(1 - ((1 + interest / 1200) ** (term * -1))));
+    let totalAmount = totalMonthPayment * term;
+    let totInterest = totalAmount - loan;
+    return data = {
+        loan,
+        totalMonthPayment,
+        totalAmount,
+        totInterest
+    }
 }
 
 
 //displays the data
-function displayData() {
+function displayData(data) {
+
+    document.getElementById("monthPays").innerHTML = ` $${data.totalMonthPayment.toFixed(2)}`;
+    document.getElementById("principal").innerHTML = ` $${data.loan.toFixed(2)}`;
+    document.getElementById("interested").innerHTML = `$${data.totInterest.toFixed(2)}`;
+    document.getElementById("costTotal").innerHTML = `$${data.totalAmount.toFixed(2)}`;
+
 
 }
